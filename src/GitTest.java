c88fd3944b3b92333fde770808decfb6c41e03cf
Irek Piątkowski <irekp@onet.pl>
2019-11-16 00:23:02 +0100

    Dodnie wersji v1.0

--- src/GitTest.java
+++ src/GitTest.java
@@ -2,5 +2,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Witaj w świecie GITa");
         System.out.println("GIT jest git!!!");
+        System.out.println("Wersja programu: v1.0");
     }
 }
